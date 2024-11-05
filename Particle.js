d4b794c13e8f34808293cb02554b0e1ada4f7bea
minssokim <minssokim6@gmail.com>
2024-11-05 14:25:35 +0900

    기본예제p5js.시작

---
+++ Particle.js
@@ -0,0 +1,40 @@
+class Particle {
+  constructor() {
+    this.pos = createVector(width/2, height/3);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+
+    this.c = color(0);
+    this.w = 50;
+  }
+
+  addForce(aForce) {
+    this.acc.add(aForce);//가속도에 힘 더하기
+  }
+
+  update() {
+    this.vel.add(this.acc);//가속하기
+    this.pos.add(this.vel);//위치에 속도 더하기
+
+    this.acc.set(0, 0);
+    this.checkEdge();
+  }
+
+
+  checkEdge() {
+    if ((this.pos.y+this.w/2) > height) {//공의 위치가 세로축 중간을 넘으면
+      this.pos.y = height-this.w/2;
+      this.vel.y = this.vel.y * -1;//속도 줄이기
+    }
+    
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+    }
+  }
+
+
+  show() {
+    fill(this.c);
+    circle(this.pos.x, this.pos.y, this.w);
+  }
+}
